a5b935bd40194b929e4d77fabcf1d147c5a1661d
julialuiza <jullia.luiza@gmail.com>
2025-07-11 10:25:57 -0400

    update server

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3500;
 
 server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
 server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
 
-server.listen(port);
+module.exports = server;
